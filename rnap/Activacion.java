42d5bdbfc15ef30e3af2d53d6544c7f3f9c50fac
Emanuel <gpe_fc@uaem.mx>
2014-03-20 08:45:10 -0600

    Proyecto Perceptron

---
+++ rnap/Activacion.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Seminario de Programacion I
+ * Emanuel GP
+ *
+ * Proyecto: Red Neuronal Artificial Perceptron 
+ ******************************************************************************/
+
+// Clase: Activacion
+
+public class Activacion{
+	private double activacionActual;
+	private double activacionAnterior;
+	private double propagacion;
+
+	public Activacion(double activacion, double propagacion){
+		activacionAnterior = activacion;
+	}
+}
---
+++ rnap/Perceptron.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+ * Seminario de Programacion I
+ * Emanuel GP
+ *
+ * Proyecto: Red Neuronal Artificial Perceptron 
+ ******************************************************************************/
+
+// Clase: Perceptron
+
+public class Perceptron{
+	private double[] entradas; // Entradas presinpaticas de la neurona.
+	private double[][] pesos; // Pesos de las conexiones sinapticas.
+	private double umbral; // Bias de la neurona.
+	private double propagacion; // Propagacion sinaptica de la neurona.
+	private double activacion; // Activacion sinaptica de la neurona.
+	private double salida; // Salida postsinaptica de la neurona.
+
+	public Perceptron(double[] entradas, double[][] pesos, double umbral){
+		this.umbral = umbral;
+		this.pesos = pesos;
+		this.entradas = entradas;
+	}
+
+	public void establecerEntradas(double[] entradas){
+		this.entradas = entradas;
+	}
+
+	public void establecerPesos(double[][] pesos){
+		this.pesos = pesos;
+	}
+
+	public double[] obtenerEntradas(){
+		return this.entradas;
+	}
+
+	public double[][] obtenerPesos(){
+		return this.pesos;
+	}
+
+	public double obtenerUmbral(){
+		return this.umbral;
+	}
+
+	public double obtenerPropagacion(){
+		return this.propagacion;
+	}
+
+	public double obtenerActivacion(){
+		return this.activacion;
+	}
+
+	public double obtenerSalida(){
+		return this.salida;
+	}
+
+	public void calcularPropagacion(){
+	}
+
+	public void calcularActivacion(){
+	}
+
+	public void calcularSalida(){
+	}
+}
