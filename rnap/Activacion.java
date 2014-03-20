66e08c9c117a954bd5e70a9de868525b9981a867
Emanuel <gpe_fc@uaem.mx>
2014-03-20 12:19:16 -0600

    Clases prototipos para la Red Perceptron

--- rnap/Activacion.java
+++ rnap/Activacion.java
@@ -14,5 +14,22 @@ public class Activacion{
 
 	public Activacion(double activacion, double propagacion){
 		activacionAnterior = activacion;
+		this.propagacion = propagacion;
+	}
+
+	public double obtenerActivacion(){
+		return activacionActual;
+	}
+
+	public void activacionIdentidad(){
+	}
+
+	public void activacionEscalonada(){
+	}
+
+	public void activacionLinealTramos(){
+	}
+
+	public void activacionSigmoidea(){
 	}
 }
---
+++ rnap/Propagacion.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * Seminario de Programacion I
+ * Emanuel GP
+ *
+ * Proyecto: Red Neuronal Artificial Perceptron 
+ ******************************************************************************/
+
+// Clase: Propagacion
+
+public class Propagacion{
+	private double umbral;
+	private double[][] pesos;
+	private double[] entradas;
+	private double propagacion;
+
+	public Propagacion(double[] entradas, double[][] pesos){}
+}
