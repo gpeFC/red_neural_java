cd10781023269e198ec7532875ba1492ae524696
Emanuel <gpe_fc@uaem.mx>
2014-04-07 04:52:44 -0500

    Red adaptada para entranar una neurona-

--- rnap/Perceptron.java
+++ rnap/Perceptron.java
@@ -8,29 +8,34 @@
 // Clase: Perceptron ...
 
 public class Perceptron{
+	private int epocas; 
+	private int errores;			// Contador de errores durante el entrenamiento.
 	private double bias;			// Umbral de activacion de la neurona.
 	private double error;			// Error de aprendizaje actual.
 	private double salida;			// Salida obtenida de la neurona.
 	private double epsilon;			// Tasa de aprendizaje del entrenamiento.
 	private double[] salidas;		// Salidas asociadas a los patrones de entrenamiento.
+	private double[] pesos;		// Patron de pesos asociados a las entradas.
 	private double[][] entradas;	// Patrones de entrenamiento de la red.
-	private double[][] pesos;		// Patron de pesos asociados a las entradas.
 
-	public Perceptron(int patrones, int argumentos, double[] entradas, double[] salidas){
-		do{
-			this.bias = (Math.random()*10 + 1)/10.0;	// Inicializacion aleatoria del bias.
-			this.epsilon = (Math.random()*10 + 1)/10.0;	// Inicializacion aleatoria de la tasa de aprendizaje.
-		}while(this.bias==0.0 || this.epsilon==0.0);
-		this.entradas = new int[dimension];
-		this.entradas = entradas;						// Asignacion del vector de las entradas.
-		this.salidas = new int[dimension];				// Asignacion del vector de las salidas.
+	public Perceptron(int patrones, int argumentos, double bias, double epsilon, double[] salidas, double[] pesos, double[][] entradas){
+		this.epocas = 0;
+		this.errores = 0;
+		this.error = 0.0;
+		this.salida = 0.0;
+		this.bias = bias;	// Inicializacion aleatoria del bias.
+		this.epsilon = epsilon;	// Inicializacion aleatoria de la tasa de aprendizaje.
+		this.salidas = new double[patrones];				// Asignacion del vector de las salidas.
 		this.salidas = salidas;
-		this.pesos = new double[patrones][argumentos];
-		for(int i=0; i<dimension; i++){
-			do{
-				this.pesos[i] = (Math.random()*10 + 1)/10.0;	// Inicializacion aleatoria de los pesos.
-			}while(this.pesos[i]==0.0);
-		}
+		this.pesos = new double[argumentos];
+		this.pesos = pesos;
+		this.entradas = new double[patrones][argumentos];
+		this.entradas = entradas;						// Asignacion del vector de las entradas.
+	}
+
+	public void imprimePesos(){
+		for(int i=0; i<this.pesos.length; i++)
+			System.out.printf("%f ", this.pesos[i]);
 	}
 
 	public void establecerBias(double bias){
@@ -41,15 +46,71 @@ public class Perceptron{
 		this.error = error;
 	}
 
-	public double[][] obtenerPesos(){
+	public void inicializarErrores(){
+		this.errores = 0;
+	}
+
+	public int obtenerEpocas(){
+		return this.epocas;
+	}
+
+	public int obtenerErrores(){
+		return this.errores;
+	}
+
+	public double obtenerError(){
+		return this.error;
+	}
+
+	public double obtenerEpsilon(){
+		return this.epsilon;
+	}
+
+	public double obtenerBias(){
+		return this.bias;
+	}
+
+	public double obtenerSalidaCalculada(){
+		return this.salida;
+	}
+
+	public double obtenerSalidaEstablecida(int indice){
+		return this.salidas[indice];
+	}
+
+	public double[] obtenerPesos(){
 		return this.pesos;
 	}
 
-	public double obtenerUmbral(){
-		return this.umbral;
+	public void calcularSalida(int indice){
+		double salida;
+		salida = 0.0;
+		for(int i=0; i<this.pesos.length; i++)
+			salida = salida + (this.pesos[i] * this.entradas[indice][i]);
+		salida = salida + bias;
+		if(salida > 0.0)
+			this.salida = 1.0;
+		else
+			this.salida = -1.0;
 	}
 
-	public double obtenerSalida(){
-		return this.salida;
+	public void actualizarBias(){
+		this.bias = this.bias + this.error;
+	}
+
+	public void actualizarErrores(){
+		this.errores++;
+	}
+
+	public void actualizarEpocas(){
+		this.epocas++;
+	}
+
+	public void actualizarPesos(int indice){
+		double[] aux_pesos = new double[this.pesos.length];
+		for(int i=0;i<this.pesos.length; i++){
+			aux_pesos[i] = this.epsilon*this.error*this.entradas[indice][i];
+			this.pesos[i] = this.pesos[i] + aux_pesos[i];
+		}
 	}
 }
--- rnap/RNAP.java
+++ rnap/RNAP.java
@@ -7,4 +7,116 @@
 
 // Clase: RNAP
 
-public class RNAP{}
+import java.util.Scanner;
+
+public class RNAP{
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int opcion=-1, num_neuronas, num_patrones, num_valores, funcion;
+		double bias, epsilon, salidas[], pesos[], prueba[], entradas[][];
+		String tipo_red, tipo_entrada;
+		Perceptron neurona;
+
+		do{
+			System.out.printf("\n\n\tRed Neuronal Artificial Perceptron...\n\n");
+			System.out.println("1) Perceptron Simple.");
+			System.out.println("2) Perceptron Multicapa.");
+			System.out.printf("\n\tTipo de Red: ");
+			opcion = entrada.nextInt();
+
+			if(opcion == 1){
+				tipo_red = "simple";
+
+				System.out.println("1) Escalon Binario(Hardlim).");
+				System.out.println("2) Escalon Bipolar(Hardlims).");
+				System.out.printf("\n\tFuncion de activacion: ");
+				opcion = entrada.nextInt();
+				if(opcion == 1)
+					funcion = 1;
+				else
+					funcion = 2;
+
+				System.out.println("1) Teclado.");
+				System.out.println("2) Archivo.).");
+				System.out.printf("\n\tEntrada de datos: ");
+				opcion = entrada.nextInt();
+				if(opcion == 1)
+					tipo_entrada = "teclado";
+				else
+					tipo_entrada = "archivo";
+
+				//System.out.printf("\nNumero de neuronas de la red: ");
+				//num_neuronas = 1;
+
+				//neuronas = new Perceptron[num_neuronas];
+
+
+				System.out.printf("\nNumero de patrones de entrenamiento: ");
+				num_patrones = entrada.nextInt();
+
+				System.out.printf("\nNumero de valores por patron: ");
+				num_valores = entrada.nextInt();
+
+				salidas = new double[num_patrones];
+				pesos = new double[num_valores];
+				entradas = new double[num_patrones][num_valores];
+
+				for(int i=0; i<num_valores; i++){
+					do{
+						pesos[i] = (Math.random()*10 + 1)/10.0;	// Inicializacion aleatoria de los pesos.
+					}while(pesos[i]==0.0);
+				}
+
+				System.out.printf("\nIngresar datos para el entrenamiento.");	
+				for(int i=0;i<num_patrones;i++){
+					for(int j=0;j<num_valores;j++){
+						System.out.printf("\nPatron[%d]-valor[%d]: ", i ,j);
+						entradas[i][j] = (double)(entrada.nextDouble());
+					}
+					System.out.printf("\nSalida asociada-Patron[%d]: ", i);
+					salidas[i] = (double)(entrada.nextDouble());
+				}
+
+				do{
+					bias = (Math.random()*10 + 1)/10.0;
+					epsilon = (Math.random()*10 + 1)/10.0;
+				}while(bias == 0.0 || epsilon == 0.0);
+
+				//for(int i=0;i<num_neuronas;i++)
+				neurona = new Perceptron(num_patrones, num_valores, bias, epsilon, salidas, pesos, entradas);
+
+				do{
+					neurona.inicializarErrores();
+					System.out.printf("\n<DATOS>...\n");
+					for(int i=0; i<num_patrones; i++){
+						neurona.calcularSalida(i);
+						neurona.establecerError(neurona.obtenerSalidaEstablecida(i) - neurona.obtenerSalidaCalculada());
+						System.out.printf("\nNeurona.");
+						System.out.printf("\n\tEpoca actual: %d", neurona.obtenerEpocas());
+						System.out.printf("\n\tError de la red: %f", neurona.obtenerError());
+						System.out.printf("\n\tBias de la neurona: %f", neurona.obtenerBias());
+						System.out.printf("\n\tTasa de aprendizaje: %f", neurona.obtenerEpsilon());
+						System.out.printf("\n\tPesos: ");
+						neurona.imprimePesos();
+						System.out.println();
+						neurona.actualizarPesos(i);
+						neurona.actualizarBias();
+						if(neurona.obtenerError() != 0.0)
+							neurona.actualizarErrores();
+					}
+					neurona.actualizarEpocas();
+				}while(neurona.obtenerErrores() != 0);
+
+				System.out.printf("\n<DATOS FINALES>...\n");
+				System.out.printf("\nNeurona.");
+				System.out.printf("\n\tEpoca de entrenamiento: %d", neurona.obtenerEpocas());
+				System.out.printf("\n\tBias de la neurona: %f", neurona.obtenerBias());
+				System.out.printf("\n\tTasa de aprendizaje: %f", neurona.obtenerEpsilon());
+				System.out.printf("\n\tPesos: ");
+				neurona.imprimePesos();
+				System.out.println();
+			}
+
+		}while(opcion != 0);
+	}
+}
